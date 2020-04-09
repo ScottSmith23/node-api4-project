390c4257ea83287e6aca5991d4935d2620eddf9b
Scott Smith <scthsm@outlook.com>
2020-04-09 15:53:29 -0600

    a

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ server.use(express.json());
 server.use('/api/users', userRouter);
 server.use('/api/posts', postRouter);
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+  res.send(`<h2>SCOTT's COOL API!</h2>`);
 });
 
 module.exports = server;
