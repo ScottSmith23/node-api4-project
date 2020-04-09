1d52c5073765ce344a8242b1c17cf57d0cad2da0
Scott Smith <scthsm@outlook.com>
2020-04-09 17:51:12 -0600

    a

--- index.js
+++ index.js
@@ -4,6 +4,4 @@ const server = require('./server.js');
 
 const port = process.env.PORT || 3333;
 
-server.listen(port, () => {
-  console.log(`\n* Server Running on ${port} *\n`);
-});
+server.listen(port, () =>console.log(`\n* Server Running on ${port} *\n`));
